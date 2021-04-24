89b908b229dcdd71df552def30a612fc1c052e82
loinhahaha <47618803+loinhahaha@users.noreply.github.com>
2021-04-24 11:01:38 +0800

    Update 03.py

--- 剑指Offer/03.py
+++ 剑指Offer/03.py
@@ -1,3 +1,9 @@
+# Find any reappear number in list.
+# Input: [2, 3, 1, 0, 2, 5, 3]
+# OutputA: 2
+# OutputB: 3
+# Tip: Use a dict as hash map
+
 class Solution:
     def findRepeatNumber(self, nums: List[int]) -> int:
         hashmap = {}
