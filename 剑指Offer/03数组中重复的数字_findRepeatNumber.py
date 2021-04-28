fc5da310a99cb840e23e4cf6ca4d2d40f130933f
loinhahaha <47618803+loinhahaha@users.noreply.github.com>
2021-04-28 12:50:42 +0800

    Update 03数组中重复的数字_findRepeatNumber.py

--- 剑指Offer/03数组中重复的数字_findRepeatNumber.py
+++ 剑指Offer/03数组中重复的数字_findRepeatNumber.py
@@ -11,4 +11,22 @@ class Solution:
             if num in hashmap:
                 return num
             hashmap[num] = 1
-    
+# TC: O(N)
+# SC: O(N)
+
+class Solution:
+    def findRepeatNumber(self, nums: [int]) -> int:
+        i = 0
+        while i < len(nums):
+            if nums[i] == i:
+                i += 1
+                continue
+            if nums[nums[i]] == nums[i]: return nums[i]
+            nums[nums[i]], nums[i] = nums[i], nums[nums[i]]
+        return -1
+# TC: O(N)
+# SC: O(1)
+
+'''
+https://leetcode-cn.com/problems/shu-zu-zhong-zhong-fu-de-shu-zi-lcof/solution/mian-shi-ti-03-shu-zu-zhong-zhong-fu-de-shu-zi-yua/
+'''
