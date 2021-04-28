1b5718a4225286a2c32d44c72fdf6bde896ec031
loinhahaha <47618803+loinhahaha@users.noreply.github.com>
2021-04-28 11:49:00 +0800

    Create 14剪绳子_cuttingRope.py

---
+++ 剑指Offer/14剪绳子_cuttingRope.py
@@ -0,0 +1,22 @@
+# The length of rope is N, can be cutted into m pieces, calculate the maximum product of these pieces.
+# Each piece length is 3 lead to maximun product.
+# Tip: https://leetcode-cn.com/problems/jian-sheng-zi-lcof/solution/mian-shi-ti-14-i-jian-sheng-zi-tan-xin-si-xiang-by/
+
+class Solution:
+    def cuttingRope(self, n: int) -> int:
+        if n < 4:
+            return n - 1
+        res = 1
+        while n > 4:
+            res *=3
+            n -= 3
+        return res * n
+
+ class Solution:
+    def cuttingRope(self, n: int) -> int:
+        if n <= 3: return n - 1
+        a, b = n // 3, n % 3
+        if b == 0: return int(math.pow(3, a))
+        if b == 1: return int(math.pow(3, a - 1) * 4)
+        return int(math.pow(3, a) * 2)
+      
